8adf8f813ef9445ef922c000ae1c51708728ea22
Damiano <dam6dam@gmail.com>
2024-10-29 09:53:40 +0100

    input component and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import Input from "./components/Input";
 
 function App() {
   return (
     <>
-      <h1>Weather App</h1>
+      <h1 className="mb-8">Weather App</h1>
+      <Input placeholder="Search for cities" />
     </>
   );
 }

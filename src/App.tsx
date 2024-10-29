b11fbd1e1de2aab1179e05315577c61ddd410319
Damiano <dam6dam@gmail.com>
2024-10-29 10:49:56 +0100

    button-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import Input from "./components/Input";
+import Button from "./components/Button";
 
 function App() {
   return (
     <>
-      <h1 className="mb-8">Weather App</h1>
-      <Input placeholder="Search for cities" />
+      <h1 className="text-white mb-8">Weather App</h1>
+      <div className="flex items-center gap-2">
+        <Input placeholder="Search for cities" />
+        <Button />
+      </div>
     </>
   );
 }

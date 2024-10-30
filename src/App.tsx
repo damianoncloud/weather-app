bed709d93af56cc8022caeb9b5d82e7783ab1624
Damiano <dam6dam@gmail.com>
2024-10-30 16:23:31 +0100

    get weather data

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,61 @@ import Input from "./components/Input";
 import Button from "./components/Button";
 // import WeatherCard from "./components/WeatherCard";
 import { ChangeEvent, useState } from "react";
+import WeatherCard from "./components/WeatherCard";
+
+interface WeatherData {
+  temperature: number;
+  windspeed: number;
+  humidity: number;
+}
 
 function App() {
   const [userInput, setUserInput] = useState<string>("");
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [currentCity, setCurrentCity] = useState<string>("");
 
   const inputHandler = (event: ChangeEvent<HTMLInputElement>) => {
     setUserInput(event.target.value);
     console.log("User input is: ", event.target.value);
   };
 
-  const getCityWeather = () => {
+  const getCityWeather = async () => {
     const city = userInput;
     console.log("City: ", city);
     const geocodingUrl = `https://nominatim.openstreetmap.org/search?q=${city}&format=json&limit=1`;
 
-    fetch(geocodingUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        const { lat, lon } = data[0];
-        console.log(`Latitudine: ${lat}, Longitudine: ${lon}`);
-        setUserInput("");
-        // getWeather(lat, lon);
-      })
-      .catch((error) => console.error("Errore nel geocoding:", error));
+    try {
+      const response = await fetch(geocodingUrl);
+      const data = await response.json();
+      const { lat, lon } = data[0];
+      console.log(`Latitudine: ${lat}, Longitudine: ${lon}`);
+
+      // Fetching WeatherData from Open Meteo API
+      const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&&hourly=relative_humidity_2m`;
+      const weatherResponse = await fetch(weatherUrl);
+      const weatherData = await weatherResponse.json();
+
+      const humidityValues = weatherData.hourly.relative_humidity_2m;
+      const dailyAverageHumidity =
+        humidityValues.reduce((acc: number, val: number) => acc + val, 0) /
+        humidityValues.length;
+
+      // Saving data in weatherData
+      setWeatherData({
+        temperature: weatherData.current_weather.temperature,
+        windspeed: weatherData.current_weather.windspeed,
+        humidity: Math.round(dailyAverageHumidity),
+      });
+
+      setCurrentCity(userInput);
+
+      console.log(weatherData);
+
+      // Input field reset
+      setUserInput("");
+    } catch (error) {
+      console.error("Geocoding error:", error);
+    }
   };
 
   return (
@@ -40,7 +72,23 @@ function App() {
         <Button onClick={getCityWeather} />
       </div>
       {userInput}
-      {/* <WeatherCard /> */}
+      {console.log(weatherData)}
+      {weatherData && (
+        <div className="weather-info">
+          <p>Temperatura: {weatherData.temperature}°C</p>
+          <p>Velocità del vento: {weatherData.windspeed} km/h</p>
+          <p>Umidità: {weatherData.humidity}%</p>
+        </div>
+      )}
+
+      {weatherData && (
+        <WeatherCard
+          temperature={weatherData.temperature}
+          windspeed={weatherData.windspeed}
+          humidity={weatherData.humidity}
+          cityName={currentCity}
+        />
+      )}
     </>
   );
 }
